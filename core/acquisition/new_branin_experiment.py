64c208da48b30149330500a3c7c3599d8dd5a3c7
juan <chucho17692@gmail.com>
2020-04-19 12:28:59 +0100

    test mode

--- core/acquisition/new_branin_experiment.py
+++ core/acquisition/new_branin_experiment.py
@@ -53,7 +53,7 @@ def function_caller_new_brannin(rep):
 
     max_iter  = 40
     # print("Finished Initialization")
-    X, Y, C, Opportunity_cost = bo.run_optimization(max_iter = max_iter,verbosity=False)
+    X, Y, C, Opportunity_cost = bo.run_optimization(max_iter = max_iter,verbosity=True)
     print("Code Ended")
 
     C_bool = np.product(np.concatenate(C, axis=1) < 0, axis=1)
