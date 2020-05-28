0e9f7b119c6e759873c39929125b68edf76a10bf
juan <chucho17692@gmail.com>
2020-05-28 17:07:44 +0100

    further tests

--- core/forking_CSC/fork0_to_csc.py
+++ core/forking_CSC/fork0_to_csc.py
@@ -114,7 +114,7 @@ ALL_CSC_NAMES = ["rilyeno", "torta", "adobo", "bulalo", "kinilaw", "okoy",
 
 # default list uses all computers, but some may need to be removed.
 # working with names is a bitch, instead use numbers (tmux panes).
-U = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+U = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 #U = [0,2,3,4,5,6,7,8]
 #U = [0,2,3,4,6,8,9,10,11,12,14]
 
