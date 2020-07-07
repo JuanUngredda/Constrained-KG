1b91270d3be89792f98586acc7fe81b7cd1940fe
juan <chucho17692@gmail.com>
2020-07-08 00:08:48 +0100

    trial

--- core/acquisition/new_branin_experiment.py
+++ core/acquisition/new_branin_experiment.py
@@ -52,7 +52,7 @@ def function_caller_new_brannin(rep):
         bo = BO(model_f, model_c, space, f, c, acquisition, evaluator, initial_design, deterministic=False)
 
 
-        max_iter  = 1
+        max_iter  = 30
         # print("Finished Initialization")
         X, Y, C, Opportunity_cost = bo.run_optimization(max_iter = max_iter,verbosity=False)
         print("Code Ended")
