33050a86f66df0ba1b59b9558c3483dc40f3e10c
JuanUngredda <j.ungredda@warwick.ac.uk>
2021-04-09 10:58:27 +0100

    hybrid cKG functions

--- core/acquisition/new_branin_hybrid_cKG_experiment.py
+++ core/acquisition/new_branin_hybrid_cKG_experiment.py
@@ -16,8 +16,9 @@ import os
 # --- Function to optimize
 print("mistery activate")
 def function_caller_new_branin(rep):
+    rep = rep+30
     np.random.seed(rep)
-    for noise in [1e-6,  1.0]:
+    for noise in [ 1.0]:
         # func2 = dropwave()
         new_brannin_f = new_brannin(sd=np.sqrt(noise))
 
