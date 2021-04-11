f3d941280a321ca9ca3a39afd24d4f5105c92baf
JuanUngredda <j.ungredda@warwick.ac.uk>
2021-04-11 13:36:21 +0100

    hybrid cKG functions

--- core/acquisition/new_branin_hybrid_cKG_experiment.py
+++ core/acquisition/new_branin_hybrid_cKG_experiment.py
@@ -18,7 +18,7 @@ print("mistery activate")
 def function_caller_new_branin(rep):
     rep = rep+30
     np.random.seed(rep)
-    for noise in [ 1.0]:
+    for noise in [ 1e-06]:
         # func2 = dropwave()
         new_brannin_f = new_brannin(sd=np.sqrt(noise))
 
@@ -68,7 +68,6 @@ def function_caller_new_branin(rep):
         print("Code Ended")
 
         print("X",X,"Y",Y, "C", C)
-
 
 # function_caller_new_branin(rep=4)
 
