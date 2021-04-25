d0858e44ca7cf108c5195b5f31d7de6833390e8d
JuanUngredda <j.ungredda@warwick.ac.uk>
2021-04-25 13:27:16 +0100

    synthetic rerun

--- core/acquisition/new_branin_hybrid_cKG_experiment.py
+++ core/acquisition/new_branin_hybrid_cKG_experiment.py
@@ -74,7 +74,7 @@ def function_caller_new_branin(rep):
         folder = "RESULTS"
         cwd = os.getcwd()
         path =cwd + "/" + folder + "/" + subfolder + '/it_' + str(rep) + '.csv'
-        X, Y, C, recommended_val, optimum, Opportunity_cost = bo.run_optimization(max_iter = max_iter,verbosity=True, path=path,
+        X, Y, C, recommended_val, optimum, Opportunity_cost = bo.run_optimization(max_iter = max_iter,verbosity=False, path=path,
                                                                                   evaluations_file=subfolder,
                                                                                   KG_dynamic_optimisation=True)
 
