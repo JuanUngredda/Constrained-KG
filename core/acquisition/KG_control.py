c61622fd769734b6a8948e2db59d0aa19c1aa139
JuanUngredda <j.ungredda@warwick.ac.uk>
2021-05-09 11:46:29 +0100

    set 2 experimetns 10/30

--- core/acquisition/KG_control.py
+++ core/acquisition/KG_control.py
@@ -53,7 +53,7 @@ def run(args):
     # IMPORT AND RUN MODULES
     #functions = [function_caller_new_brannin_TS, function_caller_test_func_2_TS, function_caller_mistery_TS, function_caller_RMITD_TS, function_caller_RMITD_EI, function_caller_RMITD]
     #functions = [function_caller_RMITD ]
-    functions = [function_caller_new_branin_v2, function_caller_mistery_v2, function_caller_test_func_2_v2]
+    functions = [function_caller_mistery_v2, function_caller_test_func_2_v2]
 
     for func in functions:
         func(args.k)
