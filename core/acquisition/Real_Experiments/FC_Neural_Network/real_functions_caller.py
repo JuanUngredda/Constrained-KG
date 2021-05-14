96ed478cb8b98cdc8425324af95bff27e1c0619c
JuanUngredda <j.ungredda@warwick.ac.uk>
2021-05-14 02:13:01 +0100

    NN exps cEI TS pKG cKG

--- core/acquisition/Real_Experiments/FC_Neural_Network/real_functions_caller.py
+++ core/acquisition/Real_Experiments/FC_Neural_Network/real_functions_caller.py
@@ -26,7 +26,7 @@ class FC_NN_test_function():
         self.rho = 0.9
         self.epsilon = 1e-07
         self.epochs = 3
-        self.samples = 500
+        self.samples = 1000
         self.num_classes = 10
         self.max_time = max_time
         # self.discrete_idx = discrete_idx
