7f14738272764576081054be5657d7f8cc0d18a6
JuanUngredda <j.ungredda@warwick.ac.uk>
2021-06-15 11:29:24 +0100

    branin noisy constraints

--- core/acquisition/KG_control.py
+++ core/acquisition/KG_control.py
@@ -58,7 +58,7 @@ def run(args):
     # IMPORT AND RUN MODULES
     #functions = [function_caller_new_brannin_TS, function_caller_test_func_2_TS, function_caller_mistery_TS, function_caller_RMITD_TS, function_caller_RMITD_EI, function_caller_RMITD]
     #functions = [function_caller_RMITD ]
-    functions = [ f1, f2, f3 , f4 , f5, f6, f7, f8, f9]
+    functions = [ f1, f2, f3 , f4 , f5, f6]
     # functions = [function_caller_NN_cKG]
     for func in functions:
         func(args.k)
