7a81d16a94764547d30f2e39f67f9af766b7ba1b
Juan Ungredda <j.ungredda@warwick.ac.uk>
2021-10-07 15:40:13 +0100

    branin# noisy constraints

--- core/acquisition/KG_control.py
+++ core/acquisition/KG_control.py
@@ -26,9 +26,11 @@ import argparse
 # from mistery_cKG_experiment import function_caller_mistery_v2
 from branin_cKG_experiment import function_caller_branin_v2
 
-from branin_nEI import function_caller_branin_nEI
-from mistery_nEI import function_caller_mistery_nEI
+# from branin_nEI import function_caller_branin_nEI
+# from mistery_nEI import function_caller_mistery_nEI
 
+from branin_TS import function_caller_new_brannin_TS
+from mistery_experiment_TS import function_caller_mistery_TS
 # This is a bare script that receives args, prints something, wastes some time,function_caller_test_func_2_TS
 # and saves something. Use this as a blank template to run experiments.
 # The sys.argv = [demo_infra_usage.py (time_stamped_folder) (integer)]
@@ -69,7 +71,7 @@ def run(args):
     # IMPORT AND RUN MODULES
     #functions = [function_caller_new_brannin_TS, function_caller_test_func_2_TS, function_caller_mistery_TS, function_caller_RMITD_TS, function_caller_RMITD_EI, function_caller_RMITD]
     #functions = [function_caller_RMITD ]
-    functions = [function_caller_branin_nEI, function_caller_mistery_nEI]
+    functions = [function_caller_new_brannin_TS, function_caller_mistery_TS]
     # functions = [function_caller_NN_cKG]
     for func in functions:
         func(args.k)
--- core/acquisition/branin_TS.py
+++ core/acquisition/branin_TS.py
@@ -15,63 +15,66 @@ from datetime import datetime
 # --- Function to optimize
 
 print("new branin TS activate")
-def function_caller_new_brannin_TS(rep):
-    rep = rep + 100
-    np.random.seed(rep)
+def function_caller_new_brannin_TS(it):
+    repepetitions = [it, it + 20]
+    for rep in repepetitions:
+        np.random.seed(rep)
+        for noise in [1]:
+            # func2 = dropwave()
+            noise_objective = noise
+            noise_constraints = (1) ** 2
+            mistery_f = new_brannin(sd_obj=np.sqrt(noise_objective), sd_c=np.sqrt(noise_constraints))
 
-    for noise in [1.0]:
-        # func2 = dropwave()
-        noise_objective = noise
-        noise_constraints = 1e-06#(0.1) ** 2
-        mistery_f = new_brannin(sd_obj=np.sqrt(noise_objective), sd_c=np.sqrt(noise_constraints))
+            # --- Attributes
+            # repeat same objective function to solve a 1 objective problem
+            f = MultiObjective([mistery_f.f])
+            c = MultiObjective([mistery_f.c])
 
-        # --- Attributes
-        # repeat same objective function to solve a 1 objective problem
-        f = MultiObjective([mistery_f.f])
-        c = MultiObjective([mistery_f.c])
+            # --- Attributes
+            # repeat same objective function to solve a 1 objective problem
 
-        # --- Attributes
-        # repeat same objective function to solve a 1 objective problem
+            # c2 = MultiObjective([test_c2])
+            # --- Space
+            # define space of variables
+            space = GPyOpt.Design_space(space=[{'name': 'var_1', 'type': 'continuous', 'domain': (-5, 10)},
+                                               {'name': 'var_2', 'type': 'continuous', 'domain': (0, 15)}])
+            n_f = 1
+            n_c = 1
+            model_f = multi_outputGP(output_dim=n_f, noise_var=[noise_objective] * n_f, exact_feval=[True] * n_f)
+            model_c = multi_outputGP(output_dim=n_c, noise_var=[noise_constraints] * n_c, exact_feval=[True] * n_c)
 
-        # c2 = MultiObjective([test_c2])
-        # --- Space
-        # define space of variables
-        space = GPyOpt.Design_space(space=[{'name': 'var_1', 'type': 'continuous', 'domain': (-5, 10)},
-                                           {'name': 'var_2', 'type': 'continuous', 'domain': (0, 15)}])
-        n_f = 1
-        n_c = 1
-        model_f = multi_outputGP(output_dim=n_f, noise_var=[noise_objective] * n_f, exact_feval=[True] * n_f)
-        model_c = multi_outputGP(output_dim=n_c, noise_var=[noise_constraints] * n_c, exact_feval=[True] * n_c)
+            # --- Aquisition optimizer
+            #optimizer for inner acquisition function
+            acq_opt = GPyOpt.optimization.AcquisitionOptimizer(optimizer='lbfgs', space=space, model=model_f, model_c=model_c)
+            #
+            # # --- Initial design
+            initial_design = GPyOpt.experiment_design.initial_design('latin', space, 10)
 
-        # --- Aquisition optimizer
-        #optimizer for inner acquisition function
-        acq_opt = GPyOpt.optimization.AcquisitionOptimizer(optimizer='lbfgs', space=space, model=model_f, model_c=model_c)
-        #
-        # # --- Initial design
-        initial_design = GPyOpt.experiment_design.initial_design('latin', space, 10)
+            nz = 1
+            acquisition = TS(model=model_f, model_c=model_c, nz=nz, space=space, optimizer=acq_opt)
+            evaluator = GPyOpt.core.evaluators.Sequential(acquisition)
+            bo = BO(model_f, model_c, space, f, c, acquisition, evaluator, initial_design,
+                    deterministic=False)
 
-        nz = 1
-        acquisition = TS(model=model_f, model_c=model_c, nz=nz, space=space, optimizer=acq_opt)
-        evaluator = GPyOpt.core.evaluators.Sequential(acquisition)
-        bo = BO(model_f, model_c, space, f, c, acquisition, evaluator, initial_design,
-                deterministic=False)
+            max_iter = 100
+            stop_date = datetime(2022, 5, 9, 7)  # year month day hour
+            # print("Finished Initialization")
+            subfolder = "new_brannin_TS_n_obj_" + str(noise_objective) + "_n_c_" + str(noise_constraints)
+            folder = "RESULTS"
+            cwd = os.getcwd()
+            path =cwd + "/" + folder + "/" + subfolder + '/it_' + str(rep) + '.csv'
+            X, Y, C, recommended_val, optimum, Opportunity_cost = bo.run_optimization(max_iter = max_iter,
+                                                                                      verbosity=False,
+                                                                                      path=path,
+                                                                                      stop_date=stop_date,
+                                                                                      compute_OC=True,
+                                                                                      evaluations_file=subfolder,
+                                                                                      KG_dynamic_optimisation=False)
+            print("Code Ended")
 
-        max_iter = 100
-        stop_date = datetime(2022, 5, 9, 7)  # year month day hour
-        # print("Finished Initialization")
-        subfolder = "new_brannin_TS_n_obj_" + str(noise_objective) + "_n_c_" + str(noise_constraints)
-        folder = "RESULTS"
-        cwd = os.getcwd()
-        path = cwd + "/" + folder + "/" + subfolder + '/it_' + str(rep) + '.csv'
-        X, Y, C, recommended_val, optimum, Opportunity_cost = bo.run_optimization(max_iter=max_iter, verbosity=False,
-                                                                                  stop_date=stop_date,
-                                                                                  path=path, evaluations_file=subfolder,
-                                                                                  KG_dynamic_optimisation=False)
-        print("Code Ended")
-
-        print("X", X, "Y", Y, "C", C)
+            print("X", X, "Y", Y, "C", C)
 
 
-function_caller_new_brannin_TS(rep=15)
+# function_caller_new_brannin_TS(15)
 
 
